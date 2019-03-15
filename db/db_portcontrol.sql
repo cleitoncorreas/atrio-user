93fe325115c64403de95c6b0baa4cdcf3aaa0696
Cleiton Corrêa <34515963+cleitoncorreas@users.noreply.github.com>
2019-03-15 18:01:22 -0300

    db

---
+++ db/db_portcontrol.sql
@@ -0,0 +1,160 @@
+-- --------------------------------------------------------
+-- Servidor:                     10.2.224.16
+-- Versão do servidor:           10.1.30-MariaDB - mariadb.org binary distribution
+-- OS do Servidor:               Win64
+-- HeidiSQL Versão:              10.1.0.5464
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Copiando estrutura para tabela db_portcontrol.chaves
+CREATE TABLE IF NOT EXISTS `chaves` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `descricao` varchar(200) NOT NULL,
+  `situacao` enum('A','I') NOT NULL DEFAULT 'A',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Exportação de dados foi desmarcado.
+-- Copiando estrutura para tabela db_portcontrol.eventos_chaves
+CREATE TABLE IF NOT EXISTS `eventos_chaves` (
+  `id` int(11) NOT NULL,
+  `id_eventos` int(11) NOT NULL,
+  `chave` int(11) NOT NULL,
+  `id_emissor` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Exportação de dados foi desmarcado.
+-- Copiando estrutura para tabela db_portcontrol.eventos_correspondecias
+CREATE TABLE IF NOT EXISTS `eventos_correspondecias` (
+  `id` int(11) NOT NULL,
+  `id_eventos` int(11) NOT NULL,
+  `código` int(11) NOT NULL,
+  `remetente` varchar(100) NOT NULL,
+  `destinatario` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Exportação de dados foi desmarcado.
+-- Copiando estrutura para tabela db_portcontrol.eventos_veiculos
+CREATE TABLE IF NOT EXISTS `eventos_veiculos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_emissor` int(11) NOT NULL,
+  `id_servico` enum('0','1','2') NOT NULL,
+  `id_motorista` int(11) NOT NULL,
+  `id_veiculo` int(11) NOT NULL,
+  `id_localidade` int(11) NOT NULL,
+  `id_status` enum('S','E') NOT NULL,
+  `id_situacao` enum('A','F') NOT NULL,
+  `data_hora_saida` datetime NOT NULL,
+  `data_hora_entrada` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Exportação de dados foi desmarcado.
+-- Copiando estrutura para tabela db_portcontrol.localidades
+CREATE TABLE IF NOT EXISTS `localidades` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `foto` varchar(50) NOT NULL DEFAULT '0',
+  `descricao` varchar(100) NOT NULL,
+  `endereco` varchar(80) NOT NULL,
+  `cidades` varchar(70) NOT NULL,
+  `telefone` varchar(25) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- Exportação de dados foi desmarcado.
+-- Copiando estrutura para tabela db_portcontrol.motoristas
+CREATE TABLE IF NOT EXISTS `motoristas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_funcionario` int(11) NOT NULL DEFAULT '0',
+  `matricula_empresa` varchar(15) NOT NULL,
+  `foto` varchar(200) NOT NULL,
+  `cnh` varchar(11) NOT NULL,
+  `validade` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+-- Exportação de dados foi desmarcado.
+-- Copiando estrutura para tabela db_portcontrol.teste
+CREATE TABLE IF NOT EXISTS `teste` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Motorista` varchar(255) NOT NULL,
+  `Carro` varchar(255) NOT NULL,
+  `Local` varchar(255) NOT NULL,
+  `Data` date NOT NULL,
+  `Motivo` varchar(512) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+-- Exportação de dados foi desmarcado.
+-- Copiando estrutura para tabela db_portcontrol.veiculos
+CREATE TABLE IF NOT EXISTS `veiculos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `marca` varchar(100) NOT NULL,
+  `modelo` varchar(100) NOT NULL,
+  `foto` varchar(200) NOT NULL,
+  `kilometragem` varchar(50) NOT NULL,
+  `placa` varchar(8) NOT NULL,
+  `local` varchar(100) NOT NULL,
+  `ano_fabricacao` int(4) NOT NULL,
+  `ano_modelo` int(4) NOT NULL,
+  `chassi` varchar(50) NOT NULL,
+  `renavam` varchar(50) NOT NULL,
+  `numero_motor` varchar(50) NOT NULL,
+  `cor` varchar(50) NOT NULL,
+  `tipo_combustivel` enum('A','G','F','D') NOT NULL,
+  `especie` varchar(50) NOT NULL,
+  `tipo_veiculo` enum('C','M') NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- Exportação de dados foi desmarcado.
+-- Copiando estrutura para view db_portcontrol.viewinfusers
+-- Criando tabela temporária para evitar erros de dependência de VIEW
+CREATE TABLE `viewinfusers` (
+	`id` INT(11) NOT NULL,
+	`nome` CHAR(50) NOT NULL COLLATE 'utf8_general_ci',
+	`usuario` CHAR(50) NOT NULL COLLATE 'utf8_general_ci',
+	`email` CHAR(50) NULL COLLATE 'utf8_general_ci',
+	`cpf` CHAR(11) NOT NULL COLLATE 'utf8_general_ci',
+	`data_nascimento` DATE NULL,
+	`telefone` CHAR(10) NULL COLLATE 'utf8_general_ci'
+) ENGINE=MyISAM;
+
+-- Copiando estrutura para view db_portcontrol.viewmotoristas
+-- Criando tabela temporária para evitar erros de dependência de VIEW
+CREATE TABLE `viewmotoristas` (
+	`id` INT(11) NOT NULL,
+	`nome` CHAR(100) NOT NULL COLLATE 'utf8_general_ci',
+	`matricula_empresa` VARCHAR(15) NOT NULL COLLATE 'utf8_general_ci',
+	`cpf` CHAR(11) NOT NULL COLLATE 'utf8_general_ci',
+	`data_nascimento` DATE NULL,
+	`telefone` CHAR(10) NULL COLLATE 'utf8_general_ci',
+	`foto` VARCHAR(200) NOT NULL COLLATE 'utf8_general_ci',
+	`cnh` VARCHAR(11) NOT NULL COLLATE 'utf8_general_ci',
+	`validade` DATE NOT NULL
+) ENGINE=MyISAM;
+
+-- Copiando estrutura para view db_portcontrol.viewinfusers
+-- Removendo tabela temporária e criando a estrutura VIEW final
+DROP TABLE IF EXISTS `viewinfusers`;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`%` SQL SECURITY DEFINER VIEW `viewinfusers` AS SELECT db_sistemas.usuarios.id,db_sistemas.usuarios.nome, db_sistemas.usuarios.usuario, db_sistemas.usuarios.email, db_sistemas.funcionarios.cpf, db_sistemas.funcionarios.data_nascimento, db_sistemas.funcionarios.telefone
+FROM db_sistemas.usuarios
+INNER JOIN db_sistemas.funcionarios ON (db_sistemas.funcionarios.nome = db_sistemas.usuarios.nome) ;
+
+-- Copiando estrutura para view db_portcontrol.viewmotoristas
+-- Removendo tabela temporária e criando a estrutura VIEW final
+DROP TABLE IF EXISTS `viewmotoristas`;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`%` SQL SECURITY DEFINER VIEW `viewmotoristas` AS SELECT db_portcontrol.motoristas.id, db_sistemas.funcionarios.nome, db_portcontrol.motoristas.matricula_empresa, db_sistemas.funcionarios.cpf, db_sistemas.funcionarios.data_nascimento, db_sistemas.funcionarios.telefone, db_portcontrol.motoristas.foto, db_portcontrol.motoristas.cnh, db_portcontrol.motoristas.validade
+FROM db_portcontrol.motoristas
+INNER JOIN db_sistemas.funcionarios ON db_portcontrol.motoristas.matricula_empresa = db_sistemas.funcionarios.matricula_empresa ;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
---
+++ db/db_sistemas.sql
@@ -0,0 +1,102 @@
+-- --------------------------------------------------------
+-- Servidor:                     10.2.224.16
+-- Versão do servidor:           10.1.30-MariaDB - mariadb.org binary distribution
+-- OS do Servidor:               Win64
+-- HeidiSQL Versão:              10.1.0.5464
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Copiando estrutura para tabela db_sistemas.funcionarios
+CREATE TABLE IF NOT EXISTS `funcionarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `matricula_fetranspor` char(15) NOT NULL,
+  `matricula_empresa` char(15) NOT NULL,
+  `pis` char(12) NOT NULL,
+  `nome` char(100) NOT NULL,
+  `cpf` char(11) NOT NULL,
+  `renda_mensal` double(9,2) NOT NULL DEFAULT '0.00',
+  `valor_diario` double(9,2) NOT NULL DEFAULT '0.00',
+  `codigo_cidade` int(2) DEFAULT '0',
+  `codigo_rede` int(2) DEFAULT '0',
+  `numero_cartao` char(13) DEFAULT NULL,
+  `tipo_cartao` int(2) NOT NULL DEFAULT '0',
+  `data_nascimento` date DEFAULT NULL,
+  `sexo` char(1) DEFAULT NULL,
+  `rg` char(15) DEFAULT NULL,
+  `orgao_emissor` char(6) DEFAULT NULL,
+  `ddd` char(3) DEFAULT NULL,
+  `telefone` char(10) DEFAULT NULL,
+  `email` char(60) DEFAULT NULL,
+  `id_unidade` int(11) DEFAULT '0',
+  `id_setor` int(11) DEFAULT '0',
+  `id_situacao` char(2) DEFAULT NULL,
+  `remember_token` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=829 DEFAULT CHARSET=utf8;
+
+-- Exportação de dados foi desmarcado.
+-- Copiando estrutura para tabela db_sistemas.migrations
+CREATE TABLE IF NOT EXISTS `migrations` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Exportação de dados foi desmarcado.
+-- Copiando estrutura para tabela db_sistemas.sistemas
+CREATE TABLE IF NOT EXISTS `sistemas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome_sistema` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- Exportação de dados foi desmarcado.
+-- Copiando estrutura para tabela db_sistemas.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `date` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Exportação de dados foi desmarcado.
+-- Copiando estrutura para tabela db_sistemas.usuarios
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` char(50) NOT NULL DEFAULT '0',
+  `usuario` char(50) NOT NULL DEFAULT '0',
+  `senha` char(255) NOT NULL DEFAULT '0',
+  `email` char(50) DEFAULT NULL,
+  `foto` char(50) DEFAULT NULL,
+  `primeiro_login` timestamp NULL DEFAULT NULL,
+  `ultimo_login` timestamp NULL DEFAULT NULL,
+  `departamento` int(11) DEFAULT '0',
+  `funcao` char(50) DEFAULT NULL,
+  `status` char(3) DEFAULT NULL,
+  `situacao` char(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+
+-- Exportação de dados foi desmarcado.
+-- Copiando estrutura para tabela db_sistemas.usuario_sistema
+CREATE TABLE IF NOT EXISTS `usuario_sistema` (
+  `id_usuario` int(11) DEFAULT NULL,
+  `id_sistema` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Exportação de dados foi desmarcado.
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
